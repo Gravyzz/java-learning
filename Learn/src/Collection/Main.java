5cbbb8e586bb0cb16220bea9bdf6306656f9f325
Zahar <zaharartamontsev@gmail.com>
2026-06-14 14:55:42 +0700

    Iterator learn

--- Learn/src/Collection/Main.java
+++ Learn/src/Collection/Main.java
@@ -4,24 +4,47 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println(removeDuplicates(new ArrayList<>(List.of(1,2,2,3,1,4,3))));  // [1,2,3,4]
-        System.out.println(removeDuplicates(new ArrayList<>(List.of(5,5,5,5))));        // [5]
-        System.out.println(removeDuplicates(new ArrayList<>(List.of(1,2,3))));          // [1,2,3]
-        System.out.println(removeDuplicates(new ArrayList<>(List.of())));               // []
-        System.out.println(removeDuplicates(new ArrayList<>(List.of(3,1,2,1,3))));      // [3,1,2]
+// removeShortWords
+        List<String> allShort = new ArrayList<>(List.of("a", "go", "hi"));
+        removeShortWords(allShort, 3);
+        System.out.println(allShort);   // [] все удалены
+
+        List<String> noShort = new ArrayList<>(List.of("code", "java"));
+        removeShortWords(noShort, 3);
+        System.out.println(noShort);    // [code, java] без изменений
+
+// removeNegatives
+        List<Integer> allNeg = new ArrayList<>(List.of(-1, -2, -3));
+        System.out.println(removeNegatives(allNeg) + " " + allNeg);  // 3 []
+
+        List<Integer> noNeg = new ArrayList<>(List.of(1, 2, 3));
+        System.out.println(removeNegatives(noNeg) + " " + noNeg);    // 0 [1, 2, 3]
 
     }
 
-    public static List<Integer> removeDuplicates(List<Integer> input){
-        Set<Integer> seen = new HashSet<>();
-        List<Integer> result = new ArrayList<>();
-        for (Integer number : input) {
-            if (!seen.contains(number)) {
-                seen.add(number);
-                result.add(number);
+    public static void removeShortWords(List<String> words, int minLength){
+
+        Iterator<String> it = words.iterator();
+        while (it.hasNext()) {
+            String word = it.next();
+            if (word.length() < minLength) {
+                it.remove();
             }
         }
-        return result;
+
     }
 
-}
+
+
+    public static int removeNegatives(List<Integer> numbers) {
+        int countRemove = 0;
+        Iterator<Integer> it = numbers.iterator();
+        while (it.hasNext()) {
+            Integer number = it.next();
+            if (number < 0) {
+                it.remove();
+                countRemove++;
+            }
+    }
+        return countRemove;
+}}
