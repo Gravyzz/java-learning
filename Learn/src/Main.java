36086e282bd2129bbb0ec0db6be59d12aa863bcf
Zahar <zaharartamontsev@gmail.com>
2026-08-28 15:45:44 +0700

    Я снова в деле

--- Learn/src/Main.java
+++ Learn/src/Main.java
@@ -1,12 +1,23 @@
-import Repetition.Student;
-
-import java.util.*;
-
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main{
-    public static void main() {
-       Student student = new Student("Alex", 19, 4.6);
-        student.printInfo();
+    public static void main(String[] args) {
+        List<Integer> source = List.of(10, 20, 30);
+        List<Number> destination = new ArrayList<>();
+
+        copy(source, destination);
+
+        System.out.println(destination);
+    }
+
+    static <T> void copy(List<? extends T> source, List<? super T> destination ){
+
+        for (T sourceObject : source) {
+            destination.add(sourceObject);
+        }
+
 
     }
+
 }
