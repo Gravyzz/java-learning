7b84d66999365389cb487c42a3c2dc3e18d67296
Zahar <zaharartamontsev@gmail.com>
2026-09-02 12:15:01 +0700

    Update Main.java

--- Learn/src/repetition/oop/Main.java
+++ Learn/src/repetition/oop/Main.java
@@ -3,11 +3,20 @@ package repetition.oop;
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println(describe(10) );
+        System.out.println(describe("java") );
+        System.out.println(describe(10,20) );
 
     }
 
-    static String describe(int value)
-    static String describe(String value)
-    static String describe(int first, int second)
+    static String describe(int value){
+        return "number: " + value;
+    }
+    static String describe(String value){
+        return "text: " + value;
+    }
+    static String describe(int first, int second){
+        return "sum: " + (first+second);
+    }
 
 }
