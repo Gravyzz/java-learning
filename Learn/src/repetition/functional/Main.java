bfd2f47b700befd4083168412328e4b07d2965f3
Zahar <zaharartamontsev@gmail.com>
2026-09-04 21:52:34 +0700

    work work work

--- Learn/src/repetition/functional/Main.java
+++ Learn/src/repetition/functional/Main.java
@@ -4,24 +4,16 @@ import java.util.Locale;
 
 public class Main {
     public static void main(String[] args) {
-        TextFormatter normalizer = text -> text.trim().toLowerCase(Locale.ROOT);
-
-        TextFormatter formatter = text -> {
-            System.out.println("format");
-            return text.trim();
-        };
-
-
-        String result = formatter.format(" Java ");
-        System.out.println(result);
 
         TextFormatter upper = text -> text.trim().toUpperCase(Locale.ROOT);
         TextFormatter brackets = text -> {
                 return  "[" + text + "]";
         };
+        TextFormatter exclamation = text -> text + "!";
 
         System.out.println(applyFormatter("  java  ", upper));
         System.out.println(applyFormatter("java", brackets));
+        System.out.println(applyFormatter("java", exclamation));
 
     }
 
