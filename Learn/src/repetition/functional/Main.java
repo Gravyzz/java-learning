f1b05a023ee9e3ca2a8dc1b8fe2d3e08c349981f
Zahar <zaharartamontsev@gmail.com>
2026-09-05 13:04:15 +0700

    ))

--- Learn/src/repetition/functional/Main.java
+++ Learn/src/repetition/functional/Main.java
@@ -3,21 +3,29 @@ package repetition.functional;
 import java.util.List;
 import java.util.Locale;
 import java.util.function.*;
+import java.util.stream.*;
 
 public class Main {
     public static void main(String[] args) {
 
-        List<Integer> numbers = List.of(-2, 0, 3, 5, -1, 3);
-        List<Integer> result = numbers.stream()
-                .filter(number -> number > 0)
-                .map(number -> number * 2)
-                .toList();
+        List<Integer> numbers = List.of(1, 2, 3);
 
+        Stream<Integer> stream = numbers.stream()
+                .filter(number -> {
+                    System.out.println("filter " + number);
+                    return number >= 2;
+                })
+                .map(number -> {
+                    System.out.println("map " + number);
+                    return number * 10;
+                });
+
+        System.out.println("before");
+
+        List<Integer> result = stream.toList();
 
         System.out.println(result);
-        System.out.println(numbers);
+
+
     }
-
-
-
 }
